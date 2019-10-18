8d79b4ab8c7965babd8b28b4cd043c27f1e81667
Natan Felles <natanfelles@gmail.com>
2019-10-18 15:26:28 -0300

    Add PHP Docs

--- src/Logger.php
+++ src/Logger.php
@@ -7,19 +7,47 @@
  */
 class Logger
 {
+	/**
+	 * Emergency level
+	 */
 	public const EMERGENCY = 7;
+	/**
+	 * Alert level
+	 */
 	public const ALERT = 6;
+	/**
+	 * CRITICAL level
+	 */
 	public const CRITICAL = 5;
+	/**
+	 * Error level
+	 */
 	public const ERROR = 4;
+	/**
+	 * Warning level
+	 */
 	public const WARNING = 3;
+	/**
+	 * Notice level
+	 */
 	public const NOTICE = 2;
+	/**
+	 * Info level
+	 */
 	public const INFO = 1;
+	/**
+	 * Debug level
+	 */
 	public const DEBUG = 0;
 	/**
+	 * Logs directory path
+	 *
 	 * @var string
 	 */
 	protected $directory;
 	/**
+	 * Active log level
+	 *
 	 * @var int
 	 */
 	protected $level = 2;
@@ -45,6 +73,8 @@ class Logger
 	}
 
 	/**
+	 * Logs with an arbitrary level.
+	 *
 	 * @param int    $level
 	 * @param string $message
 	 * @param array  $context
@@ -64,41 +94,116 @@ class Logger
 		return $this->write($message);
 	}
 
+	/**
+	 * Detailed debug information.
+	 *
+	 * @param string $message
+	 * @param array  $context
+	 *
+	 * @return bool
+	 */
 	public function debug(string $message, array $context = []) : bool
 	{
 		return $this->log(static::DEBUG, $message, $context);
 	}
 
+	/**
+	 * Interesting events.
+	 *
+	 * Example: User logs in, SQL logs.
+	 *
+	 * @param string $message
+	 * @param array  $context
+	 *
+	 * @return bool
+	 */
 	public function info(string $message, array $context = []) : bool
 	{
 		return $this->log(static::INFO, $message, $context);
 	}
 
+	/**
+	 * Normal but significant events.
+	 *
+	 * @param string $message
+	 * @param array  $context
+	 *
+	 * @return bool
+	 */
 	public function notice(string $message, array $context = []) : bool
 	{
 		return $this->log(static::NOTICE, $message, $context);
 	}
 
+	/**
+	 * Exceptional occurrences that are not errors.
+	 *
+	 * Example: Use of deprecated APIs, poor use of an API, undesirable things
+	 * that are not necessarily wrong.
+	 *
+	 * @param string $message
+	 * @param array  $context
+	 *
+	 * @return bool
+	 */
 	public function warning(string $message, array $context = []) : bool
 	{
 		return $this->log(static::WARNING, $message, $context);
 	}
 
+	/**
+	 * Runtime errors that do not require immediate action but should typically
+	 * be logged and monitored.
+	 *
+	 * @param string $message
+	 * @param array  $context
+	 *
+	 * @return bool
+	 */
 	public function error(string $message, array $context = []) : bool
 	{
 		return $this->log(static::ERROR, $message, $context);
 	}
 
+	/**
+	 * Critical conditions.
+	 *
+	 * Example: Application component unavailable, unexpected exception.
+	 *
+	 * @param string $message
+	 * @param array  $context
+	 *
+	 * @return bool
+	 */
 	public function critical(string $message, array $context = []) : bool
 	{
 		return $this->log(static::CRITICAL, $message, $context);
 	}
 
+	/**
+	 * Action must be taken immediately.
+	 *
+	 * Example: Entire website down, database unavailable, etc. This should
+	 * trigger the SMS alerts and wake you up.
+	 *
+	 * @param string $message
+	 * @param array  $context
+	 *
+	 * @return bool
+	 */
 	public function alert(string $message, array $context = []) : bool
 	{
 		return $this->log(static::ALERT, $message, $context);
 	}
 
+	/**
+	 * System is unusable.
+	 *
+	 * @param string $message
+	 * @param array  $context
+	 *
+	 * @return bool
+	 */
 	public function emergency(string $message, array $context = []) : bool
 	{
 		return $this->log(static::EMERGENCY, $message, $context);
