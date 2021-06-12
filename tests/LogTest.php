bbc470e4d8126faa561eed2ecd6506787d86c107
Natan Felles <natanfelles@gmail.com>
2021-06-12 18:18:14 -0300

    Update Coding Standard

--- tests/LogTest.php
+++ tests/LogTest.php
@@ -3,7 +3,7 @@
 use Framework\Log\Log;
 use PHPUnit\Framework\TestCase;
 
-class LogTest extends TestCase
+final class LogTest extends TestCase
 {
 	protected Log $log;
 
@@ -16,18 +16,18 @@ class LogTest extends TestCase
 		);
 	}
 
-	public function testProperties()
+	public function testProperties() : void
 	{
-		$this->assertSame('/tmp/logs/2021-06-10.log', $this->log->filename);
-		$this->assertSame('2021-06-10', $this->log->date);
-		$this->assertSame('01:55:15', $this->log->time);
-		$this->assertSame('DEBUG', $this->log->levelName);
-		$this->assertSame('abc123', $this->log->id);
-		$this->assertSame('foo bar', $this->log->message);
-		$this->assertTrue($this->log->written);
+		self::assertSame('/tmp/logs/2021-06-10.log', $this->log->filename);
+		self::assertSame('2021-06-10', $this->log->date);
+		self::assertSame('01:55:15', $this->log->time);
+		self::assertSame('DEBUG', $this->log->levelName);
+		self::assertSame('abc123', $this->log->id);
+		self::assertSame('foo bar', $this->log->message);
+		self::assertTrue($this->log->written);
 	}
 
-	public function testInvalidProperty()
+	public function testInvalidProperty() : void
 	{
 		$this->expectException(\Error::class);
 		$this->expectExceptionMessage(
@@ -36,9 +36,9 @@ class LogTest extends TestCase
 		$this->log->foo;
 	}
 
-	public function testToString()
+	public function testToString() : void
 	{
-		$this->assertSame(
+		self::assertSame(
 			'01:55:15 DEBUG abc123 foo bar',
 			(string) $this->log
 		);
