f6d6b4c27379b8d7a93bc950c37fe1dd0cc8c3f5
Natan Felles <natanfelles@gmail.com>
2021-06-28 11:42:45 -0300

    Coding Standard: Add header comments

--- src/Log.php
+++ src/Log.php
@@ -1,4 +1,13 @@
-<?php namespace Framework\Log;
+<?php
+/*
+ * This file is part of The Framework Log Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Framework\Log;
 
 /**
  * Class Log.
--- tests/LoggerMock.php
+++ tests/LoggerMock.php
@@ -1,4 +1,13 @@
-<?php namespace Tests\Log;
+<?php
+/*
+ * This file is part of The Framework Log Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\Log;
 
 use Framework\Log\Logger;
 
